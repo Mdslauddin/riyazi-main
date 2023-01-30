02fb79ebdf622809cc112e3fd89915d6eebb971b
Mdslauddin <mdslauddin285@gamil.com>
2023-01-30 10:29:51 +0530

    __all__ manage

--- riyazi/constants/codatamath.py
+++ riyazi/constants/codatamath.py
@@ -1,9 +1,18 @@
 
 __all__ = ['pi','degree','e','tue','eta','phi','euler','catalan','apery','khinchin','glaisher',
 'mertens','twinprime','yotta','zetta','exa','peta','tera','giga','mega','kilo','hecto','deka',
-           
-  
-           
+'deci','centi','milli','micro','nano','pico','femto','atto','zepto','yocto',
+'kibi','mebi','gebi','tebi','pebi','exbi','zebi','yobi','sqrt2','sqrt3','sqrt5','silver_ration',
+'cbrt2','cbrt3','supergolden_ratio','imag','chl','kepler_Bouwkamp','wallis','ln2',
+'lemniscate','Euler_constant','Erdos_Borwein','omega','laplace_limit','ramanujan_solden',
+'Gauss','gauss','second_hermit','liouviles','Fcp','ramanujan_constant','Glaisher_Kinkelin',
+'dottie_number','Meissel_Mertens','universal_parabolic','cahens','Gelfonds_consant',
+'Gelfond_Schneider','Second_Favard','Golden_angle','Sierpinskis','Levys1','Levys2',
+'Copeland_Erdos','Mills','Gompertz','Van_der_Pauw','Magic_angle','Artins','Porters','Lochs ',
+'DeViccis_tesseract','Liebs_square_ice','Nivens','Stephens','Rps','RFc','Feigenbaum' ,
+'Chaitins' ,'Robbins','Weierstrass', 'Fransen_Robinson' , 'Feigenbaum_alpha' ,'Sdbr',
+'ETF', 'Conways','HSM','Backhouses' ,'Viswanath' ,'Komornik_Loreti'  ,'Embree_Trefethen',
+'Heath_Brown_Moroz' ,'MRB' ,'Prime' ,'Sqr' ,'Foias' , 'Lcud','Taniguchi'
 ]
 
 
@@ -77,31 +86,39 @@ yobi = 2**80
 sqrt2 = 1.41421356237309504880
 sqrt3 = 1.73205080756887729352 
 sqrt5 = 2.23606797749978969640
+
 silver_ration = 2.41421356237309504880
 cbrt2 = 1.25992104989487316476
 cbrt3 = 1.44224957030740838232
 supergolden_ratio = 1.4655712318767680266
 imag = 0+1j
-connective_hexagonal_lattice = 1.84775906502257351225
+
+chl = 1.84775906502257351225 # connective_hexagonal_lattice
 kepler_Bouwkamp = 0.11494204485329620070
 wallis  =2.09455148154232659148
 ln2 = 0.69314718055994530941
 lemniscate = 2.62205755429211981046
+
+
 Euler_constant = 0.57721566490153286060
 Erdos_Borwein  = 1.60669515241529176378
 Omega = 0.56714329040978387299
 laplace_limit = 0.66274341934918158097
 ramanujan_solden = 1.45136923488338105028
+
 Gauss  = 0.83462684167407318628
+gauss = Gauss
 second_hermit = 1.15470053837925152901
 liouviles = 0.110001000000000000000001
-First_continued_fraction = 0.69777465796400798201
+Fcp = 0.69777465796400798201 # First_continued_fraction
 ramanujan_constant = 262537412640768743.999999999999250073
+
 Glaisher_Kinkelin = 1.28242712910062263687
 dottie_number = 0.73908513321516064165
 Meissel_Mertens = 0.26149721284764278375
 universal_parabolic = 2.29558714939263807403
 cahens = 0.64341054628833802618 
+
 Gelfonds_consant = 23.1406926327792690057
 Gelfond_Schneider = 2.66514414269022518865
 Second_Favard = 1.23370055013616982735 
@@ -113,35 +130,42 @@ Levys2 = 3.27582291872181115978
 Copeland_Erdos  =  0.23571113171923293137
 Mills  = 1.30637788386308069046
 Gompertz  = 0.59634736232319407434
+
 Van_der_Pauw  = 4.53236014182719380962
 Magic_angle= 0.955316618124509278163
 Artins  = 0.37395581361920228805
 Porters = 1.4670780794339754728
 Lochs  = 0.97027011439203392574
+
 DeViccis_tesseract  = 1.00743475688427937609
 Liebs_square_ice  = 1.53960071783900203869
 Nivens  = 1.7052111401053677642
 Stephens  = 0.57595996889294543964
-Regular_paperfolding_sequence = 0.85073618820186726036
-Reciprocal_Fibonacci_constant = 3.35988566624317755317
+Rps = 0.85073618820186726036 # Regular_paperfolding_sequence
+
+RFc = 3.35988566624317755317 # Reciprocal_Fibonacci_constant
 Feigenbaum  = 4.66920160910299067185
 Chaitins = 0.0078749969978123844
 Robbins  = 0.66170718226717623515
 Weierstrass = 0.47494937998792065033
+
 Fransen_Robinson   = 2.80777024202851936522
 Feigenbaum_alpha = 2.50290787509589282228
-Second_du_Bois_Reymond  = 0.19452804946532511361
-Erdos_Tenenbaum_Ford  = 0.86071332055934206887
+Sdbr  = 0.19452804946532511361 # Second_du_Bois_Reymond
+ETF  = 0.86071332055934206887 # Erdos_Tenenbaum_Ford
 Conways  = 1.30357726903429639125
-Hafner_Sarnak_McCurley = 0.35323637185499598454
+
+HSM = 0.35323637185499598454 # Hafner_Sarnak_McCurley
 Backhouses  = 1.45607494858268967139
 Viswanath  = 1.1319882487943
 Komornik_Loreti  = 1.78723165018296593301
 Embree_Trefethen  = 0.70258
+
 Heath_Brown_Moroz  = 0.00131764115485317810
 MRB  = 0.18785964246206712024
 Prime  = 0.41468250985111166024
-Somos_quadratic_recurrence  = 1.66168794963359412129
+Sqr  = 1.66168794963359412129 # Somos_quadratic_recurrence
 Foias  = 1.18745235112650105459 
-Logarithmic_capacity_of_the_unit_disk = 0.59017029950804811302
+
+Lcud = 0.59017029950804811302 # Logarithmic_capacity_of_the_unit_disk
 Taniguchi  = 0.6782344919173919780
